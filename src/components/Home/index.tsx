8029b25985558afc024eec49c2bf5a4d89a1c3b7
Eduardo Pedrosa <epedrosa@dxainvest.com>
2022-11-18 15:28:07 -0300

    criando o header da home

---
+++ src/components/Home/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+//import * as Styled from './style';
+import TheHeader from '../TheHeader';
+
+function HomePage() {
+  return <TheHeader />;
+}
+
+export default HomePage;
---
+++ src/components/TheHeader/style.ts
@@ -0,0 +1,64 @@
+import styled from 'styled-components';
+
+export const HeaderBg = styled.header`
+  background-color: #111111;
+  height: 100px;
+  display: grid;
+  align-items: center;
+
+  @media (max-width: 769px) {
+    height: auto;
+    padding-bottom: 32px;
+    padding-top: 20px;
+  }
+`;
+
+export const HeaderContent = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 60px;
+
+  @media (max-width: 769px) {
+    flex-direction: column;
+    padding: 0 20px;
+  }
+`;
+
+interface ButtonProps {
+  outline: boolean;
+}
+
+export const HeaderButton = styled.button`
+  font-family: 'Poppins', 'sans-serif';
+  font-size: 14px;
+  color: ${(p: ButtonProps) => (p.outline ? '#ffffff' : '#000000')};
+  background-color: ${(p: ButtonProps) =>
+    p.outline ? 'transparent' : '#ffffff'};
+  border: ${(p: ButtonProps) => (p.outline ? '2px solid #ffffff' : '')};
+  width: 144px;
+  height: 52px;
+  transition: 0.4s;
+  border-radius: 8px;
+  letter-spacing: 0.015rem;
+
+  :hover {
+    border-color: ${(p: ButtonProps) => (p.outline ? '#FFDD00' : '#f22')};
+    box-shadow: ${(p: ButtonProps) => (p.outline ? '' : '0 0 0 4px #ffffff')};
+  }
+
+  :active {
+    box-shadow: ${(p: ButtonProps) =>
+      p.outline ? '0 0 0 6px #FFDD00' : '0 0 0 6px #ffffff'};
+  }
+`;
+
+export const ButtonsFlex = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 20px;
+
+  @media (max-width: 769px) {
+    margin-top: 32px;
+  }
+`;
