dc0fe6fad91037e1110aad8ee1b291b1823dd144
Thiago Tibau <ttibaudev@gmail.com>
2022-12-29 11:37:06 -0300

    calendar feature

---
+++ src/components/BarberCalendar/index.tsx
@@ -0,0 +1,5 @@
+import { CalendarComponent } from 'components/BarberCalendar/Calendar';
+
+export const BarberCalendar = () => {
+  return <CalendarComponent />;
+};
--- src/pages/profile/calendar.tsx
+++ src/pages/profile/calendar.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import * as Styled from '../../styles/Admin/calendar';
 import AdminDrawer from '../../components/BarberAdmin/Drawer';
 import AdminHeader from '../../components/BarberAdmin/Header';
+import { BarberCalendar } from 'components/BarberCalendar';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
 
 function AdminCalendar() {
   return (
@@ -11,6 +13,7 @@ function AdminCalendar() {
       <Styled.CalendarTitle>
         Here is the Component the calendar
       </Styled.CalendarTitle>
+      <BarberCalendar />
     </>
   );
 }
